20a30e0bc92e1b17834c56f0ccc97efcf8d7ae04
AE <valefleur@gmail.com>
2020-01-28 13:32:07 -0800

    Added query to display all jobs.  Unit tests currently failing due to rigid testing requirements.

--- jobs/app.py
+++ jobs/app.py
@@ -30,4 +30,9 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    query = '''SELECT job.id, job.title, job.description. job.salary,
+    employer.id as employer_id, employer.name as employer_name
+    FROM job
+    JOIN employer ON employer.id = job.employer_id'''
+    jobs = execute_sql(query)
+    return render_template('index.html', jobs=jobs)
